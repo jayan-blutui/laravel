ea16d97a4c472516cdd8f55c1d136b7a35fb664b
Jayan Ratna <jayan@blutui.com>
2022-01-27 08:40:52 +1300

    add main route to pass example tests

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return [
+        'user' => route('user'),
+    ];
+});
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+})->name('user');
